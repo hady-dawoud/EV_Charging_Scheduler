b8ac7fd6298b596adf01e9a3a728830c679f79bd
Hady <hady.sherif@msa.edu.eg>
2026-06-01 16:56:22 +0300

    chore(mobile): switch hosted API URL to HTTPS

--- apps/mobile/src/services/api.ts
+++ apps/mobile/src/services/api.ts
@@ -22,7 +22,7 @@ const LOCAL_API_BASE_URL =
     ? 'http://10.0.2.2:8000'
     : 'http://127.0.0.1:8000';
 
-const HOSTED_API_BASE_URL = 'http://smartevcharging.uaenorth.cloudapp.azure.com/api';
+const HOSTED_API_BASE_URL = 'https://smartevcharging.uaenorth.cloudapp.azure.com/api';
 
 const IS_DEV_BUILD = typeof __DEV__ !== 'undefined' && __DEV__;
 
